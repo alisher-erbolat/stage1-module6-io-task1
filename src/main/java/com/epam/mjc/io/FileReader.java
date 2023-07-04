2293703fe8efd40d81c8d8331eb45e0582c15c8f
alisher-erbolat <alisher.001017@gmail.com>
2023-07-04 22:55:19 +0600

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,18 +2,17 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file){
+    public static Profile getDataFromFile(File file){
 
-        String[] allStr = new String[10];
+        String[] allStr = new String[5];
 
         try (FileInputStream fileInputStream = new FileInputStream(file)){
-            String str = "";
+            StringBuilder str = new StringBuilder();
             int ch;
             boolean add = false;
             while((ch=fileInputStream.read())!=-1){
@@ -24,14 +23,15 @@ public class FileReader {
                     add = false;
                 }
                 else if (add){
-                    str += (char) ch;
+                    str.append((char)ch);
                 }
             }
-            allStr = str.trim().split(" ");
+            str = new StringBuilder(str.substring(1));
+            System.out.println(str);
+            allStr = str.toString().split(" ");
         } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            ex.printStackTrace();
         }
         return new Profile(allStr[0],Integer.parseInt(allStr[1]),allStr[2],Long.parseLong(allStr[3]));
     }
-
 }
